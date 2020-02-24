a2ba101855c0845ad9dbb80ac9ad5859699d0905
zhaoqi <1223996281@qq.com>
2020-02-24 14:01:38 +0800

    TxUtility

--- src/main/java/com/company/keystore/wallet/TxUtility.java
+++ src/main/java/com/company/keystore/wallet/TxUtility.java
@@ -1732,11 +1732,6 @@ public class TxUtility extends Thread {
      */
     public static JSONObject CreateMultipleForRuleFirst(String fromPubkeyStr,  byte[] assetHash,int min, int max, List<byte[]> pubList, List<byte[]> signatures,BigDecimal amount){
         try {
-            amount = amount.multiply(BigDecimal.valueOf(rate));
-            JSONObject jsonObjectAmount = isValidPositiveLong(amount);
-            if(jsonObjectAmount.getInteger("code") == 5000){
-                return jsonObjectAmount;
-            }
             Multiple multiple = new Multiple(assetHash, min, max,pubList,signatures,amount.longValue());
             //版本号
             byte[] version = new byte[1];
